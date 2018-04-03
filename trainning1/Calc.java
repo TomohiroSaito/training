a8d24982379b5959ceb66956141c5fe8838dc02e
TomohiroSaito <saito@genuine-pt.jp>
2018-04-03 15:20:08 +0900

    'Javaコンソール出力課題1-3'

--- trainning1/Calc.java
+++ trainning1/Calc.java
@@ -1,14 +1,40 @@
 class Calc {
 	public static void main(String args[]) {
-		try {
-			double ans = 0;
-			for(int i=0; i<args.length; i++) {
-				ans += Double.parseDouble(args[i]);
+		if(args.length < 3) {
+			System.out.println("引数が足りません。");
+		} else if(args.length > 3) {
+			System.out.println("引数が多すぎます。");
+		} else {
+			int flg = 1;
+			double num1 = 0;
+			double num2 = 0;
+			try {
+				num1 = Double.parseDouble(args[0]);
+				flg++;
+				num2 = Double.parseDouble(args[1]);
+				String ope = args[2];
+				double ans = 0;
+				if(ope.equals("+")) {
+					ans = num1 + num2;
+					System.out.println("計算結果は、" + ans + "です。");
+				} else if(ope.equals("-")) {
+					ans = num1 - num2;
+					System.out.println("計算結果は、" + ans + "です。");
+				} else if(ope.equals("*")) {
+					ans = num1 * num2;
+					System.out.println("計算結果は、" + ans + "です。");
+				} else if(ope.equals("/")) {
+					ans = num1 / num2;
+					System.out.println("計算結果は、" + ans + "です。");
+				} else if(ope.equals("%")) {
+					ans = num1 % num2;
+					System.out.println("計算結果は、" + ans + "です。");
+				} else {
+					System.out.println("入力できる記号は、「+」、「-」、「*」、「/」、「%」です。");
+				}
+			} catch(NumberFormatException e) {
+				System.out.println(flg + "番目の引数は数値で入力してください。");
 			}
-			String strans = String.format("入力された値の合計は%fです。", ans);
-			System.out.println(strans);
-		} catch (NumberFormatException e) {
-			System.out.println("数字で入力してください。");
 		}
 	}
 }
