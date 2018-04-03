c2e1d4004b3911b6f98f17e32119aa9d78758eba
TomohiroSaito <saito@genuine-pt.jp>
2018-04-03 13:55:23 +0900

    'Javaコンソール出力課題1-2応用'

--- trainning1/HelloWorld.java
+++ trainning1/HelloWorld.java
@@ -1,14 +1,13 @@
-import java.io.*;
-
 class HelloWorld {
 	public static void main(String args[]) {
-		try{
-				int ans = 0;
-				for(int i=0; i<args.length; i++) {
-					ans += Integer.parseInt(args[i]);
-				}
-				System.out.println("入力された値の合計は" + ans + "です。");
-		} catch(NumberFormatException e) {
+		try {
+			double ans = 0;
+			for(int i=0; i<args.length; i++) {
+				ans += Double.parseDouble(args[i]);
+			}
+			String strans = String.format("入力された値の合計は%fです。", ans);
+			System.out.println(strans);
+		} catch (NumberFormatException e) {
 			System.out.println("数字で入力してください。");
 		}
 	}
