fee4d162963ecb5d74259b891721e4378aa76e45
TomohiroSaito <saito@genuine-pt.jp>
2018-04-09 14:48:16 +0900

    'javaDB接続課題1-1訂正3'

--- src/training2/CreateStudents.java
+++ src/training2/CreateStudents.java
@@ -3,13 +3,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Calendar;
 
 import training2.studentmodel.party.Party;
 import training2.studentmodel.party.PartyName;
@@ -24,7 +17,8 @@ public class CreateStudents {
 		outMessage(2);
 		String studentClass = inputClass();
 		Student student = new Student(new StudentName(studentName), new Party(new PartyName(studentClass)));
-		insertStudent(student);
+		StudentDataAccess studentDataAccess = new StudentDataAccess();
+		studentDataAccess.insertStudent(student);
 		outMessage(3);
 	}
 
@@ -81,110 +75,6 @@ public class CreateStudents {
 			System.exit(-1);
 		}
 
-	}
-
-	static void insertStudent(Student student) {
-		int result = 0;
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-
-		//クラス名から、クラスIDをデータベースより取得
-		String studentName = student.getStudentName().getName();
-		Student resultStudent = selectClassId(student);
-
-		int classId = resultStudent.getParty().getPartyId().getId();
-
-		try {
-			connection = getConnection();
-
-			Date date = new Date(Calendar.getInstance().getTimeInMillis());
-
-			//INSERT文の実行
-			String sql = "INSERT INTO Student (class, name, created_at, updated_at) VALUES(?, ?, ?, ?)";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, classId);
-			preparedStatement.setString(2, studentName);
-			preparedStatement.setDate(3, date);
-			preparedStatement.setDate(4, date);
-			result = preparedStatement.executeUpdate();
-			connection.commit();
-			insertError(result);
-		} catch(SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-				if(connection != null) connection.close();
-			} catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	//executeUpdateメソッドの戻り値が0だったら、エラーメッセージを表示
-	static void insertError(int result) {
-		if(result == 0) {
-			System.out.println("何らかの原因で登録できませんでした。");
-			System.exit(-1);
-		}
-	}
-
-	static Student selectClassId(Student student) {
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-
-		String className = student.getParty().getPartyName().getName();
-
-		try {
-			connection = getConnection();
-
-			//SELECT文の実行
-			String sql = "SELECT class_id FROM Class WHERE class_name=?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, className);
-			resultSet = preparedStatement.executeQuery();
-			while(resultSet.next()) {
-				int classId = resultSet.getInt("class_id");
-				student.getParty().getPartyId().setId(classId);
-			}
-		} catch(SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(preparedStatement != null) preparedStatement.close();
-				if(resultSet != null) resultSet.close();
-				if(connection != null) connection.close();
-			} catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return student;
-	}
-
-	static Connection getConnection() {
-		Connection connection = null;
-
-		//接続文字列
-		String url = "jdbc:postgresql://localhost:5433/postgres";
-		String user = "postgres";
-		String password = "postgres";
-
-		try {
-			Class.forName("org.postgresql.Driver");//ClassNotFoundException投げる
-
-			//PostgreSQLへ接続
-			connection = DriverManager.getConnection(url, user, password);
-
-			//自動コミットOFF
-			connection.setAutoCommit(false);
-
-		} catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
-		return connection;
 	}
 
 }
---
+++ src/training2/StudentDataAccess.java
@@ -0,0 +1,111 @@
+package training2;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+
+import training2.studentmodel.student.Student;
+
+public class StudentDataAccess {
+
+	public Connection getConnection() {
+		Connection connection = null;
+
+		//接続文字列
+		String url = "jdbc:postgresql://localhost:5433/postgres";
+		String user = "postgres";
+		String password = "postgres";
+
+		try {
+			Class.forName("org.postgresql.Driver");//ClassNotFoundException投げる
+
+			//PostgreSQLへ接続
+			connection = DriverManager.getConnection(url, user, password);
+
+			//自動コミットOFF
+			connection.setAutoCommit(false);
+
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+	public int insertStudent(Student student) {
+		int result = 0;
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		//クラス名から、クラスIDをデータベースより取得
+		String studentName = student.getStudentName().getName();
+		Student resultStudent = selectClassId(student);
+
+		int classId = resultStudent.getParty().getPartyId().getId();
+
+		try {
+			connection = getConnection();
+
+			Date date = new Date(Calendar.getInstance().getTimeInMillis());
+
+			//INSERT文の実行
+			String sql = "INSERT INTO Student (class, name, created_at, updated_at) VALUES(?, ?, ?, ?)";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, classId);
+			preparedStatement.setString(2, studentName);
+			preparedStatement.setDate(3, date);
+			preparedStatement.setDate(4, date);
+			result = preparedStatement.executeUpdate();
+			connection.commit();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(preparedStatement != null) preparedStatement.close();
+				if(connection != null) connection.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public Student selectClassId(Student student) {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		String className = student.getParty().getPartyName().getName();
+
+		try {
+			connection = getConnection();
+
+			//SELECT文の実行
+			String sql = "SELECT class_id FROM Class WHERE class_name=?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, className);
+			resultSet = preparedStatement.executeQuery();
+			while(resultSet.next()) {
+				int classId = resultSet.getInt("class_id");
+				student.getParty().getPartyId().setId(classId);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(preparedStatement != null) preparedStatement.close();
+				if(resultSet != null) resultSet.close();
+				if(connection != null) connection.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return student;
+	}
+
+}
